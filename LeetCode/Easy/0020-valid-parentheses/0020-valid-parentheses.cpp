4a9f5e72557942796505e0cdc8d5a09d3da50ddd
Suryansh Sharma <suryanshsharma310106@gmail.com>
2026-09-22 18:29:17 +0530

    Time: 0 ms (100%), Space: 9 MB (36.93%) - LeetHub

---
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int i=0;
+        while(i<s.size()){
+            if(s[i]=='('||s[i]=='{'|| s[i]=='[') st.push(s[i]);
+            else if(s[i]==')' && !st.empty() && st.top()=='(') st.pop();
+            else if(s[i]=='}' && !st.empty() && st.top()=='{') st.pop();
+            else if(s[i]==']' && !st.empty() && st.top()=='[') st.pop();
+            else return false;
+            i++;
+            
+        }
+        return st.empty() ? true:false;
+    }
+};
